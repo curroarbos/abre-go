19371a28fe4b0b6cb592043c0207f61597fbe0d4
BrendanSmith <brendansmith464@gmail.com>
2023-11-28 16:29:52 +0800

    Fixed profile pic and minor style changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_24_045833) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_074958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -110,7 +110,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_24_045833) do
     t.string "last_name"
     t.text "photo_url"
     t.boolean "is_provider"
-    t.text "phone_number"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
