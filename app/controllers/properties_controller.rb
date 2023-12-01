c4d1603a03d38c09710a2b795ecc2590cef1f5b2
BrendanSmith <brendansmith464@gmail.com>
2023-12-01 12:41:34 +0800

    Can add a property from an airbnb link

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,9 +1,11 @@
+require "json"
+require "open-uri"
+
 class PropertiesController < ApplicationController
 
   def show
     # Have the user_id only
-    @user = User.find(params[:id])
-    @property = Property.where(user_id: @user.id).first
+    @property = Property.find(params[:id])
     @recommendations = Recommendation.where(property_id: @property.id)
   end
 
@@ -12,9 +14,31 @@ class PropertiesController < ApplicationController
   end
 
   def create
-    @property = Property.new(property_params)
+    @property = Property.new
+
+    full_url = params[:property][:embedded_link]
+    match_data = full_url.match(/https:\/\/www.airbnb.com\/rooms\/(\d+)/)
+    property_id = match_data[1]
+    url = URI("https://airbnb-data.p.rapidapi.com/WebAPIs/airbnb/detail?id=#{property_id}&locale=en&currency=USD")
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(url)
+    request["X-RapidAPI-Key"] = ENV["AIRBNB_RAPID_API_KEY"]
+    request["X-RapidAPI-Host"] = 'airbnb-data.p.rapidapi.com'
+    response = http.request(request).read_body
+    details = JSON.parse(response)
+
+    lat = details["data"]["metadata"]["loggingContext"]["eventDataLogging"]["listingLat"]
+    lng = details["data"]["metadata"]["loggingContext"]["eventDataLogging"]["listingLng"]
+    @property.name = details["data"]["metadata"]["seoFeatures"]["ogTags"]["ogDescription"]
+    @property.photo = details["data"]["metadata"]["sharingConfig"]["imageUrl"]
+    @property.location = "#{lat},#{lng}"
+    @property.embedded_link = full_url
+    @property.user = current_user
+
     if @property.save
       redirect_to property_path(@property)
+      current_user.update(is_owner: true)
     else
       render :new
     end
