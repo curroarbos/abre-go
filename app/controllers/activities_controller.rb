7f0e802d7f6c8406486c052b8e142a9fd82445cc
milaura22@gmail.com <milaura22@gmail.com>
2023-12-04 15:36:35 +0800

    property id passed to search and bookngs

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -67,7 +67,7 @@ class ActivitiesController < ApplicationController
   private
 
   def activity_params
-    params.require(:activity).permit(:title, :location, :max_people, :price, :photo_url, :category_id, :photo)
+    params.require(:activity).permit(:title, :location, :max_people, :price, :photo_url, :category_id, :photo, :property_id)
   end
 
   def set_activity
