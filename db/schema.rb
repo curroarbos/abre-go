e8f2f3c21de06ea1df78ad2c19ad7ae234b6ec5f
BrendanSmith <brendansmith464@gmail.com>
2023-12-05 17:32:48 +0800

    added description to new and edit for activities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_04_080840) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_05_092624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_04_080840) do
     t.bigint "category_id"
     t.boolean "in_house"
     t.float "duration"
+    t.text "description"
     t.index ["category_id"], name: "index_activities_on_category_id"
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
