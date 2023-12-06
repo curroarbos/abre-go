0403f0542781636a540cde6cda081635193dea79
Francisco Arbos Ortega <curroarbos@gmail.com>
2023-12-06 13:11:54 +0800

    fixed style of config page, added params[:property_id] for booking and possibility to destroy timeslot configuration

--- app/controllers/days_time_slots_controller.rb
+++ app/controllers/days_time_slots_controller.rb
@@ -1,13 +1,14 @@
 class DaysTimeSlotsController < ApplicationController
 
   before_action :set_config_time_slot, only: %i[new create]
+  before_action :set_days_time_slot, only: %i[destroy]
 
   def new
     @days_time_slot = DaysTimeSlot.new
   end
 
   def create
-    @days_time_slot = DaysTimeSlot.new(day_time_slot_params)
+    @days_time_slot = DaysTimeSlot.new(days_time_slot_params)
     @days_time_slot.config_time_slot = @config_time_slot
     if @days_time_slot.save
       redirect_to config_time_slot_path(@config_time_slot)
@@ -16,13 +17,23 @@ class DaysTimeSlotsController < ApplicationController
       end
   end
 
+  def destroy
+    @days_time_slot.destroy
+    @config_time_slot = @days_time_slot.config_time_slot
+    redirect_to config_time_slot_path(@config_time_slot)
+  end
+
   private
 
   def set_config_time_slot
     @config_time_slot = ConfigTimeSlot.find(params[:config_time_slot_id])
   end
 
-  def day_time_slot_params
+  def days_time_slot_params
     params.require(:days_time_slot).permit(:start_time, :interval, :frequency, day: [])
+  end
+
+  def set_days_time_slot
+    @days_time_slot = DaysTimeSlot.find(params[:id])
   end
 end
